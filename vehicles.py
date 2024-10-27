0fd0715d84181632fbda82f79552e8a344103bfd
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2024-10-27 18:02:05 +0100

    update

--- vehicles.py
+++ vehicles.py
@@ -14,22 +14,22 @@ from vehicle import Vehicle
 
 
 class Vehicles():
-    __slots__ = ("__vehicles")
+    __slots__ = ("__previous_mapping", "__vehicles")
 
     def __init__(self):
         self.__vehicles = {}
 
     def process(self, current_boxes, previous_boxes):
-        ordered_diatances = {}
+        ordered_distances = {}
 
         for current_box in current_boxes:
-            ordered_diatances[current_box.identifier] = PriorityQueue()
+            ordered_distances[current_box.identifier] = PriorityQueue()
 
             for previous_box in previous_boxes:
                 dist = centroids_distance(
-                    np.array([current_box.centroid]), np.array([previous_box.centroid]))
+                    np.array([current_box.centroid]), np.array([previous_box.centroid]))[0, 0]
 
-                ordered_diatances[current_box.identifier].put_nowait(
+                ordered_distances[current_box.identifier].put_nowait(
                     (dist, previous_box.identifier))
 
-        return ordered_diatances
+        # TODO
